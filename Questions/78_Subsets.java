7ea771642e9f0337a6145b250cad17152ad974ab
YuanshuCai <caiyuanshu@gmail.com>
2024-01-25 09:37:59 -0500

    add DFS questions and some other udpates

--- Questions/78_Subsets.java
+++ Questions/78_Subsets.java
@@ -5,10 +5,10 @@ Space Complexity: O(n)
      */
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        generateSubsets(nums, 0, new ArrayList<>(), ans);
+        generateSubsets1(nums, 0, new ArrayList<>(), ans);
         return ans;
     }
-     private void generateSubsets(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
+    private void generateSubsets1(int[] nums, int index, List<Integer> current, List<List<Integer>> result) {
         result.add(new ArrayList<>(current));
         for (int i = index; i < nums.length; i++) {
             current.add(nums[i]);
@@ -16,4 +16,27 @@ Space Complexity: O(n)
             current.remove(current.size() - 1); //subtracking
         }
     }
+    
 }
+
+/*
+generateSubsets1
+
+                [""]
+            /          \
+           [a]           ""
+          /   \          /   \
+       [ab]    a      [b]     ""
+        |       |      |       |
+     [abc]    [ac]     [bc]    [c]
+*/
+/* generateSubsets2 2
+                      ""
+            /                \
+           ""                   c
+          /   \                /   \
+        ""       b            ""     bc
+      /  \     /  \          /  \    /  \
+  [""]  [a]   [b] [ab]    [c]  [ac] [bc] [abc]
+
+*/
